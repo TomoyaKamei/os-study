f33fc87cb18e3273a40d7adeb46b8f3846f498ee
Janus022012 <kamei022012@gmail.com>
2022-05-24 22:00:25 +0900

    2022/05/24 InterpretorInGo

--- Book/WritingAnInterpreterInGo/code/monkey/ast/ast.go
+++ Book/WritingAnInterpreterInGo/code/monkey/ast/ast.go
@@ -1,10 +1,14 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 // Node ASTの要素を指す。
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement Nodeの一種で、文を表す。
@@ -33,6 +37,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements{
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // LetStatement let <Identifier> = <Expression>を表し、Node・Statement(文)インターフェースを充足する。
 type LetStatement struct {
 	Token token.Token // token.LETトークン
@@ -42,6 +56,21 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string{
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil{
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 // Identifier 識別子を表し、Node・Expression(式)インターフェースを充足する。
 type Identifier struct {
@@ -51,6 +80,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
 
 // ReturnStatement return <expression>を表し、Node・Statement(文)インターフェースを充足する。
 type ReturnStatement struct {
@@ -60,3 +90,31 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string{
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil{
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement 式文を表し、Node・Statement(文)インターフェースを充足する。
+type ExpressionStatement struct{
+	Token  token.Token		//式の最初のトークン
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {return es.Token.Literal}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil{
+		return es.Expression.String()
+	}
+	return ""
+}
